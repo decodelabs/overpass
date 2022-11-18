26185e2c43589e1db818334d7a0b13634aaacc31
Tom Wright <tom@inflatablecookie.com>
2022-11-18 19:51:52 +0000

    Updated evaluator
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/evaluate.js
+++ src/evaluate.js
@@ -1,3 +1,6 @@
+var Module=require("module").Module,nodePath=require("path"),appModulePaths=[],old_nodeModulePaths=Module._nodeModulePaths;function addPath(a,o){function e(o){a=nodePath.normalize(a),o&&-1===o.indexOf(a)&&o.push(a)}if(a=nodePath.normalize(a),-1===appModulePaths.indexOf(a))for(appModulePaths.push(a),require.main&&e(require.main.paths),o=o||module.parent;o&&o!==require.main;)e(o.paths),o=o.parent}Module._nodeModulePaths=function(a){var o=old_nodeModulePaths.call(this,a);return o.concat(appModulePaths)};
+addPath(process.cwd() + '/node_modules');
+
 var input = '';
 
 process.stdin.on('data', function (chunk) {
